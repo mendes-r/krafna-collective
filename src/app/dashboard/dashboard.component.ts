29b4b4367a24bcedea1cfb610af22511bdfa299c
Ricardo Mendes <mendes.r@gmx.com>
2021-10-23 22:27:56 +0100

    Add hover effect with project name and mouse hover event

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,10 +9,20 @@ import { ProjectService } from '../service/project.service';
 })
 export class DashboardComponent implements OnInit {
   portfolio!: Project[];
+  hoverProject: string = '';
 
   constructor(private _service: ProjectService) {}
 
   ngOnInit(): void {
     this.portfolio = this._service.getPortfolio();
+  }
+
+  /**
+   * Sets hoverProject value.
+   * The value is displayed during a hover event on the projects panels
+   * @param name - project's name
+   */
+  setHoverProject(name: string) {
+    this.hoverProject = name;
   }
 }
--- src/app/project-panel/project-panel.component.ts
+++ src/app/project-panel/project-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Project } from '../entity/project';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-project-panel',
@@ -8,6 +9,23 @@ import { Project } from '../entity/project';
 })
 export class ProjectPanelComponent {
   @Input() project!: Project;
+  @Output() emitName = new EventEmitter<string>();
 
   constructor() {}
+
+  /**
+   * Sends project name to parent component.
+   * Connected to the project hover effect in the dashboard
+   */
+  sendName(): void {
+    this.emitName.emit(this.project.name);
+  }
+
+  /**
+   * Sends empty string to parent component.
+   * This enables that when a mouse leaves the component, the parent doesn't show the hover effect
+   */
+  sendNothing(): void {
+    this.emitName.emit("");
+  }
 }

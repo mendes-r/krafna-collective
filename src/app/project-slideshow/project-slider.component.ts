b2756133d5aecb489f942fed94764e1e05b154d4
Ricardo Mendes <mendes.r@gmx.com>
2021-10-25 10:04:33 +0100

    Send data to child component when using routerLink

--- src/app/project-slideshow/project-slider.component.ts
+++ src/app/project-slideshow/project-slider.component.ts
@@ -1,10 +1,23 @@
+import { Project } from './../entity/project';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectService } from '../service/project.service';
 
 @Component({
   selector: 'app-project-slider',
   templateUrl: './project-slider.component.html',
   styleUrls: ['./project-slider.component.css'],
 })
-export class ProjectSliderComponent {
-  constructor() {}
+export class ProjectSliderComponent implements OnInit {
+  project!: Project;
+  id: string = '';
+
+  constructor(private route: ActivatedRoute, private _service: ProjectService) {}
+
+  ngOnInit(): void {
+    let temp: any;
+    this.route.paramMap.subscribe((param) => temp=param.get('id'));
+    this.id = temp || {};
+    this.project = this._service.getProject(this.id);
+  }
 }

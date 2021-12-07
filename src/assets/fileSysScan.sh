66a33fd76a7ddf352d37f23705a86086f8efc6ab
Ricardo Mendes <mendes.r@gmx.com>
2021-12-07 23:16:34 +0000

    Updated the script and add some images to the slide folder

--- src/assets/fileSysScan.sh
+++ src/assets/fileSysScan.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
 
+# Creates a JSON with an array of object representations of the different projects and associated images
+#
+# Project name: XX-PROJECT-NAME
+
 SOURCE='./images/'
-LIST_OF_PROJECTS=''
+SLIDE='/slide/'
 JSON="./projects.json"
 
 projects () {
-	cat <<- _EOF_
-{
-  "name": $(echo $1 | cut -d - -f2-)
-  "id": $(echo $1 | cut -d - -f1)
+	cat <<- _EOF_ >> $JSON
+  {
+    "name": "$(echo $1 | cut -d - -f2-)",
+    "id": $(echo $1 | cut -d - -f1),
+    "thumbnail": "$(find $SOURCE$1 -type f -depth 1 -name '*.jpg' -o -name '*.png' | head -n 1)",
+    "slideshow": [
 	_EOF_
 	return
 }
 
-thumbnail () {
-  cat <<- _EOF_
-  "thumbnail": $(find $SOURCE$1 -type f -depth 1 -name '*.jpg' -o -name '*.png' | head -n 1)
+slides () {
+	cat <<- _EOF_ >> $JSON
+			# Know if is the last iteration to remove the coma
+			# https://stackoverflow.com/questions/12298261/how-to-know-if-file-in-a-loop-is-the-last-one
+      "$1",
 	_EOF_
 	return
 }
 
-slideshow () {
-  cat <<- _EOF_
-  "slideshow": {
+if [ -f $JSON ]; then
+	rm $JSON
+fi
 
-  }
-},
-	_EOF_
-	return
-}
-
+echo '[' >> $JSON
 for project in $(ls $SOURCE)
 do
   projects $project
-  thumbnail $project
-  slideshow
+
+	for slide in $(find $SOURCE$project$SLIDE -type f -depth 1 -name '*.jpg' -o -name '*.png')
+	do
+		slides $slide
+	done
+
+	echo "    ]" >> $JSON
+
+	# Know if is the last iteration to remove the coma
+	# https://stackoverflow.com/questions/12298261/how-to-know-if-file-in-a-loop-is-the-last-one
+	echo "  }," >> $JSON
 done
+echo "]" >> $JSON

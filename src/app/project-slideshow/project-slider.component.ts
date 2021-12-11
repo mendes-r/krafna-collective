c33c11dfdfa8e343c802367ad12a87ecd6ccbead
Ricardo Mendes <mendes.r@gmx.com>
2021-12-11 23:06:41 +0000

    Sketch first simple slide show

--- src/app/project-slideshow/project-slider.component.ts
+++ src/app/project-slideshow/project-slider.component.ts
@@ -1,5 +1,5 @@
 import { Project } from './../entity/project';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectService } from '../service/project.service';
 
@@ -8,14 +8,41 @@ import { ProjectService } from '../service/project.service';
   templateUrl: './project-slider.component.html',
   styleUrls: ['./project-slider.component.css'],
 })
-export class ProjectSliderComponent implements OnInit {
+export class ProjectSliderComponent implements OnInit, AfterViewInit {
   project!: Project;
   id: string = '';
+  slideIndex: number = 0;
 
-  constructor(private route: ActivatedRoute, private _service: ProjectService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private _service: ProjectService
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((param) => this.id=param.get('id') || '');
+    this.route.paramMap.subscribe((param) => (this.id = param.get('id') || ''));
     this.project = this._service.getProject(this.id);
+  }
+
+  ngAfterViewInit(): void {
+    this.showSlides();
+    console.log("FOO")
+  }
+
+  showSlides() {
+    var i;
+    var slides = document.getElementsByClassName(
+      'slide-left'
+    ) as HTMLCollectionOf<HTMLElement>;
+
+    if (this.slideIndex >= slides.length) {
+      this.slideIndex = 0;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+
+    slides[this.slideIndex].style.display = 'block';
+    this.slideIndex++;
   }
 }

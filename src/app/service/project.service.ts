881506df42f4b36ed746239261264726dfe9212a
Ricardo Mendes <mendes.r@gmx.com>
2021-12-12 18:25:03 +0000

    Add shuffle feature to change project order on each refresh

--- src/app/service/project.service.ts
+++ src/app/service/project.service.ts
@@ -9,7 +9,9 @@ export class ProjectService {
 
   data: Project[] = projects;
 
-  constructor() {}
+  constructor() {
+    this.shuffleProjects();
+  }
 
   public getProject(id: string): Project {
     return this.data.filter(elemente => elemente.id === id)[0];
@@ -17,5 +19,20 @@ export class ProjectService {
 
   public getPortfolio(): Project[] {
     return this.data;
+  }
+
+  /**
+   * Randomize array in-place using Durstenfeld shuffle algorithm.
+   *
+   * source: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+   */
+   private shuffleProjects() {
+    let dataSize: number = this.data.length;
+    for (let i = dataSize - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = this.data[i];
+      this.data[i] = this.data[j];
+      this.data[j] = temp;
+    }
   }
 }

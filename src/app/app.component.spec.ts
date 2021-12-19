3f188b36b1052bdeb56451596efffafd031b61f7
Ricardo Mendes <mendes.r@gmx.com>
2021-12-19 11:38:51 +0000

    FIX unit tests and ADD hyperlinks to social media icons

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
+import { Router, RouterModule } from '@angular/router';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -14,18 +18,28 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'krafna-collective'`, () => {
+  it(`should have all the frames`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('krafna-collective');
+    const top = fixture.debugElement.queryAll(By.css('#frame-t'))
+    const bottom = fixture.debugElement.queryAll(By.css('#frame-b'))
+    const left = fixture.debugElement.queryAll(By.css('#frame-l'))
+    const right = fixture.debugElement.queryAll(By.css('#frame-r'))
+    expect(top).toBeTruthy;
+    expect(bottom).toBeTruthy;
+    expect(left).toBeTruthy;
+    expect(right).toBeTruthy;
   });
 
-  it('should render title', () => {
+  it(`should have a social networks hyperlinks`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'krafna-collective app is running!'
-    );
+    const social = fixture.debugElement.queryAll(By.css('#social'))
+    expect(social).toBeTruthy;
   });
+
+  it(`should not have back to main page button`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const social = fixture.debugElement.queryAll(By.css('#back'))
+    expect(social).toBeFalsy;
+  });
+
 });
